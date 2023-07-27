d465626af353cff52b0d634ce78402724dae0984
Aodj05 <aodj05@msn.com>
2023-07-27 14:00:55 -0400

    Added sounds and animation

--- pySpaceInvaders.py
+++ pySpaceInvaders.py
@@ -1,23 +1,32 @@
 import pygame
+import random
 
 pygame.init()
 
 width = 800
 height = 600
 
+
+# Define colors
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+BULLET_COLOR = (255, 255, 0)
+
+screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption("Space Invaders Clone")
 
-pygame. display.update()
+clock = pygame.time.Clock()
+
+pygame.display.update()
 
 pygame.mixer.init()
-
 
 
 player_ship_img = pygame.image.load("media/Lightning.png")
 
 player_ship_width = 60
 player_ship_height = 40
-player_ship_img = pygame.transform.scale(player_ship_img, (player_ship width, player_ship_height))
+player_ship_img = pygame.transform.scale(player_ship_img, (player_ship_width, player_ship_height))
 
 invader1_img = pygame.image.load("media/Dove.png")
 
@@ -33,6 +42,10 @@ fastinvader4_sound = pygame.mixer.Sound("media/fastinvader4.wav")
 invaderkilled_sound = pygame.mixer.Sound("media/invaderkilled.wav")
 shoot_sound = pygame.mixer.Sound("media/shoot.wav")
 ufopitch_sound = pygame.mixer.Sound("media/ufo_lowpitch.wav")
+
+shooting_condition = False
+invader_destroyed = False
+player_hit = False
 
 #replace consitions to detect events
 if shooting_condition:
@@ -105,6 +118,16 @@ def display_game_over():
     game_over_text = font.render("GAME OVER", True, RED)
     screen.blit(game_over_text, (width // 2 - game_over_text.get_width() // 2, height // 2))
 
+def check_bullet_collision(bullets, invaders):
+    for bullet in bullets:
+        for invader in invaders:
+            if bullet.y <= invader.y + invader1_height and invader.x <= bullet.x <= invader.x + invader1_width:
+                # Bullet collided with an invader
+                destroy_invader(invader)
+                bullets.remove(bullet)
+                break
+
+
 score = 0
 
 player_bullets = []
@@ -118,6 +141,32 @@ invader_animation_time = 500
 current_animation_frame = 0
 last_animation_time = pygame.time.get_ticks()
 
+# Grid of the invaders
+invader_rows = 5
+invader_cols = 10
+
+invaders = []
+for row in range(invader_rows):
+    for col in range(invader_cols):
+        #spacing between invaders
+        invader_x = col * (invader1_width + 10)
+        invader_y = row * (invader1_height + 10)
+        invaders.append(Invader1(invader_x, invader_y,))
+
+score = 0
+lives = 3
+game_over = False
+
+invader_bullet_interval = 2000  # 2000 milliseconds (2 seconds)
+last_bullet_time = pygame.time.get_ticks()
+
+def display_lives():
+    font = pygame.font.Font(None, 42)
+    lives_text = font.render(f"Lives: {lives}", True, WHITE)
+    screen.blit(lives_text, (10, 50))
+
+player_ship = PlayerShip()
+
 running = True
 while running:
     for invader in invaders:
@@ -125,7 +174,11 @@ while running:
 
     # check window boundaries
     for invader in invaders:
-        if invader.x <= or invader.x + invader_width >= width:
+        if invader.x <= 0 or invader.x + invader1_width >= width:
+            invader_dir *= -1
+            for inv in invaders:
+                inv.y += 10
+
 
     # Game loop
     pygame.display.update()
@@ -138,7 +191,11 @@ while running:
             elif event.key == pygame.K_RIGHT:
                 player_ship.x += player_ship.speed
             if event.key == pygame.K_SPACE:
+                shooting_condition = True
                 player_bullets.append(Bullet(player_ship.x + player_ship.width // 2, player_ship.y))
+        #filter off-screen bullets
+    player_bullets = [bullet for bullet in player_bullets if bullet.y > 0]
+    invader_bullets = [bullet for bullet in invader_bullets if bullet.y < height]
 
     for bullet in player_bullets:
         bullet.y -= bullet.speed
@@ -146,9 +203,6 @@ while running:
     for bullet in invader_bullets:
         bullet.y += bullet.speed
 
-    #filter off-screen bullets
-    player_bullets = [bullet for bullet in player_bulllets if bullet.y > 0]
-    invader_bullets = [bullet for bullet in invader_bulllets if bullet.y < height]
 
     #screen clear
     screen.fill((0, 0, 0))
@@ -168,29 +222,28 @@ while running:
     # Update and draw the player score
     display_score()
 
+    
+
     #check for game over
     if check_game_over():
         game_over = True
 
     if game_over:
         display_game_over()
-# Grid of the invaders
-invader_rows = 5
-invader_cols = 10
+    else:
+        display_lives()
 
-invaders = []
-for row in range(invader_rows):
-    for col in range(invader_cols):
-        #spacing between invaders
-        invader_x = col * (invader_width + 10)
-        invader_y = row * (invader_height + 10)
-        invaders.append(Invader1(invader_x, invader_y,))
+player_ship = PlayerShip()
 
-    player_ship = PlayerShip()
-
-    random invader shooting
-    implement bullet collisions
+if current_time - last_bullet_time >= invader_bullet_interval:
+    last_bullet_time = current_time
+    random_invader = random.choice(invaders)
+    invader_bullets.append(Bullet(random_invader.x + invader1_width // 2, random_invader.y + invader1_height))
     
+    check_bullet_collision(player_bullets, invaders)
+    check_bullet_collision(invader_bullets, [player_ship])  # Check collision with player ship
+
+
     #draw objects
     player_ship.draw()
     for bullet in player_bullets:
@@ -200,6 +253,8 @@ for row in range(invader_rows):
         pygame.draw.rect(screen, BULLET_COLOR, (bullet.x, bullet.y, bullet.width, bullet.height))
     # update display
     pygame.display.update()
+
+    clock.tick(60)
 
 pygame.mixer.quit()
 pygame.quit()
@@ -229,7 +284,7 @@ pygame.quit()
 
 
 
-#Add Sound Effects: Use sound effects to enhance the gaming experience. Include sounds for shooting, enemy explosions, and player's ship movement.
+
 
 #Polish and Refine: Test your game thoroughly and make any necessary adjustments to improve gameplay and overall user experience.
 
